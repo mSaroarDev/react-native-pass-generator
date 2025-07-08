a986868f899bed28f4c3c86e66db37897fde221d
mSaroarDev <msaroar.dev@gmail.com>
2025-07-08 09:53:03 +0600

    ccc

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,77 +1,161 @@
 import { useState } from "react";
-import { Platform, StyleSheet, Text, TextInput, View } from "react-native";
+import { Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import CustomSwitch from "./CustomSwitch";
 import GradientButton from "./Button";
+import * as Yup from "yup";
+import { Formik } from "formik";
 
 const Main = () => {
 
-  const [inputLength, setInputLength] = useState(4);
-  const [isUppercaseEnabled, setIsUppercaseEnabled] = useState(false);
-  const [isLowercaseEnabled, setIsLowercaseEnabled] = useState(false);
-  const [isNumbersEnabled, setIsNumbersEnabled] = useState(false);
-  const [isSymbolsEnabled, setIsSymbolsEnabled] = useState(false);
+  const [generatedPassword, setGeneratedPassword] = useState("");
+
+  // const [inputLength, setInputLength] = useState(6);
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
+
+  const [isUppercaseEnabled, setIsUppercaseEnabled] = useState(true);
+  const [isLowercaseEnabled, setIsLowercaseEnabled] = useState(true);
+  const [isNumbersEnabled, setIsNumbersEnabled] = useState(true);
+  const [isSymbolsEnabled, setIsSymbolsEnabled] = useState(true);
+
+  const validationSchema = Yup.object().shape({
+    inputLength: Yup.number()
+      .min(4, "Minimum length is 4")
+      .max(16, "Maximum length is 16")
+      .required("Input length is required"),
+  });
+
+  const generatePasswordString = (passwordLength: number) => {
+    let selectedCharacters = '';
+
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+    if (isUppercaseEnabled) {
+      selectedCharacters += uppercase;
+    }
+    if (isLowercaseEnabled) {
+      selectedCharacters += lowercase;
+    }
+    if (isNumbersEnabled) {
+      selectedCharacters += numbers;
+    }
+    if (isSymbolsEnabled) {
+      selectedCharacters += symbols;
+    }
+
+    const result = createPassword(selectedCharacters, passwordLength);
+    setGeneratedPassword(result);
+    setIsPassGenerated(true);
+    console.log(`Generated Password: ${result}`);
+    
+  };
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+
+    return result;
+  }
+
+  const reset = () => {
+    setGeneratedPassword("");
+    setIsPassGenerated(false);
+    setIsUppercaseEnabled(true);
+    setIsLowercaseEnabled(true);
+    setIsNumbersEnabled(true);
+    setIsSymbolsEnabled(true);
+  }
 
   return (
-    <View style={styles.mainContainer}>
-      <View style={styles.header}>
-        <Text style={styles.text}>Password Generator</Text>
-      </View>
-      <View style={styles.passwordBox}>
-        <Text style={styles.generatedPassword}>Pswwf45&45</Text>
-      </View>
+    <Formik
+      initialValues={{ inputLength: 6 }}
+      onSubmit={values => generatePasswordString(values.inputLength)}
+      validationSchema={validationSchema}
+    >
+      {({ 
+        values, 
+        handleChange,  
+        handleSubmit, 
+        handleReset
+      }) => (
+        <View style={styles.mainContainer}>
+          <View style={styles.header}>
+            <Text style={styles.text}>Password Generator</Text>
+          </View>
 
-      <View style={styles.flexBox}>
-        <Text style={styles.inputLabel}>Input Length</Text>
-        <TextInput style={styles.inputField}
-          value={inputLength.toString()}
-          onChangeText={(text) => setInputLength(Number(text))}
-          keyboardType="numeric"
-          maxLength={2}
-          placeholderTextColor="#aaa"
-          textAlign="center"
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-      </View>
+          {isPassGenerated && (
+            <View style={styles.passwordBox}>
+              <Text style={styles.generatedPassword}>{generatedPassword}</Text>
+            </View>
+          )}
 
-      <View style={styles.flexBox}>
-        <Text style={styles.inputLabel}>Include Uppercase</Text>
-        <CustomSwitch
-          value={isUppercaseEnabled}
-          onToggle={setIsUppercaseEnabled}
-          style={styles.switch}
-        />
-      </View>
 
-      <View style={styles.flexBox}>
-        <Text style={styles.inputLabel}>Include Lowercase</Text>
-        <CustomSwitch
-          value={isLowercaseEnabled}
-          onToggle={setIsLowercaseEnabled}
-          style={styles.switch}
-        />
-      </View>
+          <View style={styles.flexBox}>
+            <Text style={styles.inputLabel}>Input Length</Text>
+            <TextInput style={styles.inputField}
+              value={String(values.inputLength)}
+              onChangeText={handleChange('inputLength')}
+              keyboardType="numeric"
+              maxLength={2}
+              placeholderTextColor="#aaa"
+              textAlign="center"
+            />
+          </View>
 
-      <View style={styles.flexBox}>
-        <Text style={styles.inputLabel}>Include Numbers</Text>
-        <CustomSwitch
-          value={isNumbersEnabled}
-          onToggle={setIsNumbersEnabled}
-          style={styles.switch}
-        />
-      </View>
+          <View style={styles.flexBox}>
+            <Text style={styles.inputLabel}>Include Uppercase</Text>
+            <CustomSwitch
+              value={isUppercaseEnabled}
+              onToggle={setIsUppercaseEnabled}
+              style={styles.switch}
+            />
+          </View>
 
-      <View style={styles.flexBox}>
-        <Text style={styles.inputLabel}>Include Symbols</Text>
-        <CustomSwitch
-          value={isSymbolsEnabled}
-          onToggle={setIsSymbolsEnabled}
-          style={styles.switch}
-        />
-      </View>
+          <View style={styles.flexBox}>
+            <Text style={styles.inputLabel}>Include Lowercase</Text>
+            <CustomSwitch
+              value={isLowercaseEnabled}
+              onToggle={setIsLowercaseEnabled}
+              style={styles.switch}
+            />
+          </View>
 
-      <GradientButton title="Generate Password" />
-    </View>
+          <View style={styles.flexBox}>
+            <Text style={styles.inputLabel}>Include Numbers</Text>
+            <CustomSwitch
+              value={isNumbersEnabled}
+              onToggle={setIsNumbersEnabled}
+              style={styles.switch}
+            />
+          </View>
+
+          <View style={styles.flexBox}>
+            <Text style={styles.inputLabel}>Include Symbols</Text>
+            <CustomSwitch
+              value={isSymbolsEnabled}
+              onToggle={setIsSymbolsEnabled}
+              style={styles.switch}
+            />
+          </View>
+
+          <GradientButton onPress={handleSubmit} title="Generate Password" />
+          <TouchableOpacity 
+            onPress={()=> {
+            reset();
+            handleReset();
+          }}
+          style={styles.resetButton}
+        >
+          <Text style={styles.resetButtonText}>Reset</Text>
+        </TouchableOpacity>
+        </View>
+      )}
+    </Formik>
   );
 };
 
@@ -140,6 +224,29 @@ const styles = StyleSheet.create({
   switch: {
     transform: Platform.OS === 'android' ? [{ scaleX: 1.2 }, { scaleY: 1.2 }] : [],
   },
+  resetButton: {
+    backgroundColor: "#ff4d4d",
+    paddingVertical: 15,
+    paddingHorizontal: 45,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,  
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  resetButtonText: {
+    color: '#fff',
+    textTransform: 'uppercase',
+    fontSize: 14,
+    fontFamily: 'Quicksand-Bold', 
+    letterSpacing: 1,
+  }
 });
 
 export default Main;
